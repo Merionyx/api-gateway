7dbfc7c496c571bf5441239f4165cb659302e17e
Mikhail Minaev <inbox@mikhailminaev.com>
2026-03-14 18:08:06 +0300

    Refactor main function by delegating control plane logic to a new package
    - Moved the main control plane logic from `main.go` to a new `internal/app/control-plane/run.go` file for better organization and separation of concerns.
    - Introduced a `Run` function to encapsulate the initialization and server startup processes, enhancing code clarity and maintainability.
    - This refactor prepares the codebase for future enhancements in control plane functionality.

--- cmd/control-plane/main.go
+++ cmd/control-plane/main.go
@@ -1,78 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log/slog"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"merionyx/api-gateway/control-plane/internal/config"
-	"merionyx/api-gateway/control-plane/internal/container"
-	git "merionyx/api-gateway/control-plane/internal/repository/git"
-	"merionyx/api-gateway/control-plane/internal/server"
+	"log"
+	controlplane "merionyx/api-gateway/control-plane/internal/app/control-plane"
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	}))
-
-	slog.SetDefault(logger)
-
-	// Load config
-	cfg, err := config.LoadConfig(os.Getenv("CONFIG_PATH"))
-	if err != nil {
-		logger.Error(fmt.Sprintf("Failed to load config: %v", err))
-		os.Exit(1)
+	if err := controlplane.Run(); err != nil {
+		log.Fatalf("Failed to run control plane: %v", err)
 	}
-	logger.Info("Config loade", "config", cfg)
-
-	// Initialize repositories
-	if err := git.InitializeRepositories(cfg.Repositories); err != nil {
-		logger.Error(fmt.Sprintf("Failed to initialize repositories: %v", err))
-		os.Exit(1)
-	}
-
-	// Initialize DI container
-	container, err := container.NewContainer(cfg)
-	if err != nil {
-		logger.Error(fmt.Sprintf("Failed to initialize container: %v", err))
-		os.Exit(1)
-	}
-	defer container.Close()
-
-	// Setup graceful shutdown
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// Start HTTP server
-	go func() {
-		if err := server.StartHTTPServer(container); err != nil {
-			logger.Error(fmt.Sprintf("HTTP server error: %v", err))
-			cancel()
-		}
-	}()
-
-	// Start gRPC server
-	go func() {
-		if err := server.StartGRPCServer(container); err != nil {
-			logger.Error(fmt.Sprintf("gRPC server error: %v", err))
-			cancel()
-		}
-	}()
-
-	// Wait for shutdown signal
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	select {
-	case <-sigChan:
-		logger.Info("Shutdown signal received")
-	case <-ctx.Done():
-		logger.Info("Context cancelled")
-	}
-
-	logger.Info("Shutting down servers...")
 }
---
+++ internal/app/control-plane/run.go
@@ -0,0 +1,78 @@
+package controlplane
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"merionyx/api-gateway/control-plane/internal/config"
+	"merionyx/api-gateway/control-plane/internal/container"
+	"merionyx/api-gateway/control-plane/internal/repository/git"
+	"merionyx/api-gateway/control-plane/internal/server"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Run() error {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	slog.SetDefault(logger)
+
+	// Load config
+	cfg, err := config.LoadConfig(os.Getenv("CONFIG_PATH"))
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to load config: %v", err))
+		os.Exit(1)
+	}
+	logger.Info("Config loade", "config", cfg)
+
+	// Initialize repositories
+	if err := git.InitializeRepositories(cfg.Repositories); err != nil {
+		logger.Error(fmt.Sprintf("Failed to initialize repositories: %v", err))
+		os.Exit(1)
+	}
+
+	// Initialize DI container
+	container, err := container.NewContainer(cfg)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to initialize container: %v", err))
+		os.Exit(1)
+	}
+	defer container.Close()
+
+	// Setup graceful shutdown
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Start HTTP server
+	go func() {
+		if err := server.StartHTTPServer(container); err != nil {
+			logger.Error(fmt.Sprintf("HTTP server error: %v", err))
+			cancel()
+		}
+	}()
+
+	// Start gRPC server
+	go func() {
+		if err := server.StartGRPCServer(container); err != nil {
+			logger.Error(fmt.Sprintf("gRPC server error: %v", err))
+			cancel()
+		}
+	}()
+
+	// Wait for shutdown signal
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-sigChan:
+		logger.Info("Shutdown signal received")
+	case <-ctx.Done():
+		logger.Info("Context cancelled")
+	}
+
+	logger.Info("Shutting down servers...")
+	return nil
+}
