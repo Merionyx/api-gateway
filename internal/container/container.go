a8e100791dc5654aafd0a41317e3955d0674b6d1
Mikhail Minaev <inbox@mikhailminaev.com>
2026-03-15 10:24:23 +0300

    Add Environment and Service structs to Container
    - Introduced new structs: Environment, EnvironmentService, EnvironmentContract, and their respective configuration types to enhance the Container's structure.
    - These additions improve the management of environment-specific services and contracts, facilitating better organization and access to related data within the Container.

--- internal/container/container.go
+++ internal/container/container.go
@@ -47,6 +47,34 @@ type Container struct {
 	Router *router.Router
 }
 
+type Environment struct {
+	Name      string
+	Snapshots []git.ContractSnapshot
+	Services  EnvironmentService
+	Contracts EnvironmentContract
+}
+
+type EnvironmentServiceConfig struct {
+	Type string
+	List []EnvironmentService
+}
+
+type EnvironmentService struct {
+	Name     string
+	Upstream string
+}
+
+type EnvironmentContractConfig struct {
+	Type string
+	List []EnvironmentContract
+}
+
+type EnvironmentContract struct {
+	Name       string
+	Repository string
+	Ref        string
+}
+
 // NewContainer creates and initializes a new DI container
 func NewContainer(cfg *config.Config) (*Container, error) {
 	container := &Container{
